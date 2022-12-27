0ba7ffa0e542a468b96d0df6d4dbb9edebd7e4e7
nos64 <manosov1984@gmail.com>
2022-12-27 11:44:46 +0400

    feat: add base routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+
+import MainPage from '../../pages/MainPage';
+import NotFoundPage from '../../pages/NotFoundPage';
+import Layout from '../../components/Layout';
+
+import { ROUTES } from '../../common/routes';
 
 function App() {
-  return <div className="App">Burger!!!</div>;
+  return (
+    <Routes>
+      <Route path={ROUTES.MAIN} element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path={ROUTES.NOTFOUND} element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
