b8a81fa4ffa68f6a4dc047b2b1362aed4d6f4f6d
nos64 <manosov1984@gmail.com>
2023-01-19 15:42:10 +0400

    refactor: change Loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,12 +4,23 @@ import styles from './Loader.module.scss';
 
 const Loader = () => {
   return (
-    <div className={styles.loaderWrapper}>
-      <div className={styles.loader}>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className={styles.load}>
+      <div className={styles.hamburger}>
+        <div className={styles['top-bun']}></div>
+        <div className={styles.pickle}></div>
+        <div className={styles.pickle}></div>
+        <div className={styles.tomato}>
+          <div></div>
+        </div>
+        <div className={styles.tomato}>
+          <div></div>
+        </div>
+        <div className={styles.cheese}></div>
+        <div className={styles.cheese}></div>
+        <div className={styles.beef}></div>
+        <div className={styles['bottom-bun']}></div>
       </div>
+      <h1>Loading...</h1>
     </div>
   );
 };
