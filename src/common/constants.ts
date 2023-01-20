bed55bd55f85cc1e68cd224818ab66a4036b6181
nos64 <manosov1984@gmail.com>
2023-01-20 08:50:16 +0400

    refactor: change API adres

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,5 @@
-export const API_URL = 'http://localhost:3024';
-// export const API_URL = 'https://methed-burger-api.onrender.com';
+// export const API_URL = 'http://localhost:3024';
+export const API_URL = 'https://methed-burger-api.onrender.com';
 export const PREFIX_PRODUCT = '/api/product';
 
 export const ORDER_API = 'https://reqres.in/api/users';
