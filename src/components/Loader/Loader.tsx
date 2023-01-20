4c793cff56504e0c80170c6af18e3c983be9beaf
nos64 <manosov1984@gmail.com>
2023-01-20 15:06:19 +0400

    feat: add Loader wrapper

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,23 +4,25 @@ import styles from './Loader.module.scss';
 
 const Loader = () => {
   return (
-    <div className={styles.load}>
-      <div className={styles.hamburger}>
-        <div className={styles['top-bun']}></div>
-        <div className={styles.pickle}></div>
-        <div className={styles.pickle}></div>
-        <div className={styles.tomato}>
-          <div></div>
+    <div className={styles.loaderWrapper}>
+      <div className={styles.load}>
+        <div className={styles.hamburger}>
+          <div className={styles['top-bun']}></div>
+          <div className={styles.pickle}></div>
+          <div className={styles.pickle}></div>
+          <div className={styles.tomato}>
+            <div></div>
+          </div>
+          <div className={styles.tomato}>
+            <div></div>
+          </div>
+          <div className={styles.cheese}></div>
+          <div className={styles.cheese}></div>
+          <div className={styles.beef}></div>
+          <div className={styles['bottom-bun']}></div>
         </div>
-        <div className={styles.tomato}>
-          <div></div>
-        </div>
-        <div className={styles.cheese}></div>
-        <div className={styles.cheese}></div>
-        <div className={styles.beef}></div>
-        <div className={styles['bottom-bun']}></div>
+        <h1>Loading...</h1>
       </div>
-      <h1>Loading...</h1>
     </div>
   );
 };
